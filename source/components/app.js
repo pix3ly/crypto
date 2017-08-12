ee41d82d067e11514738c18e0312a33cb876487e
Daniël van Drunen <daniel@pixely.me>
2017-08-12 13:27:19 +0200

    Add base currency

--- source/components/app.js
+++ source/components/app.js
@@ -3,9 +3,24 @@ import React from 'react'
 import Currency from './currency'
 
 export default class App extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            base: {
+                currency: 'USD',
+                value: 1
+            }
+        }
+    }
+
     render() {
         return (
             <div>
+                <div>
+                    <h1>{this.state.base.value}</h1>
+                    {this.state.base.currency}
+                </div>
                 <Currency name="Test" value="300" />
             </div>
         )
