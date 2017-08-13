fbe6eb66ea8d2774a27ef5389c4a0133e3f5ee24
Daniël van Drunen <daniel@pixely.me>
2017-08-13 14:50:33 +0200

    Multiply quotes's value by base's value

--- source/components/app.js
+++ source/components/app.js
@@ -36,7 +36,7 @@ export default class App extends React.Component {
 
         copiedQuotes.forEach((quote) => {
             axios.get('https://api.bitfinex.com/v2/tickers?symbols=t' + quote.name + this.state.base.name).then((response) => {
-                quote.value = Number(response.data[0][7]).toFixed(2)
+                quote.value = response.data[0][7]
 
                 this.setState({ ...this.state, quotes: copiedQuotes })
             })
@@ -53,7 +53,7 @@ export default class App extends React.Component {
                 <div>
                     {this.state.quotes.map((quote) => {
                         return (
-                            <Currency name={quote.name} value={quote.value} />
+                            <Currency name={quote.name} value={Number(quote.value * this.state.base.value).toFixed(2)} />
                         )
                     })}
                 </div>
